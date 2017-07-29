b6ef9efaee5cf9aa4b2c5356912320f6a2a78139
Kate Morgenstern <katemorg51@gmail.com>
2017-07-29 12:52:32 -0400

    initial push, structure

---
+++ db/seedDB.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) 
+VALUES 
+('burger name 1'), 
+('burger name 2'), 
+('burger name 3'),
+('burger name 4'), 
+('burger name 5'), 
+('burger name 6'), 
+('burger name 7'), 
+('burger name 8');
